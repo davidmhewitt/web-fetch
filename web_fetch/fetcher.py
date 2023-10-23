a01235d44068f14c415d37ba0ec670a662f434e9
David Hewitt <davidmhewitt@users.noreply.github.com>
2023-10-23 20:19:02 +0000

    Download images and map src attributes in HTML

--- web_fetch/fetcher.py
+++ web_fetch/fetcher.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 from dataclasses import dataclass
 
 import aiofiles
@@ -26,14 +27,14 @@ async def write_to_file(filename: str, text: str) -> None:
         raise RuntimeError(f"Failed to write to {filename}: {e}") from e
 
 
-async def fetch(url: str, session: aiohttp.ClientSession) -> FetchResult:
+async def fetch(url: str, output_path: str, session: aiohttp.ClientSession) -> FetchResult:
     """
     Fetch a URL and return a tuple of the response text and the HTTP host that
     returned the response (useful for identifying the request).
 
     :param url: URL to fetch
+    :param output_path: path to folder to write files into
     :param session: aiohttp session
-    :param fetch_metadata: whether to fetch metadata
     :return: FetchResult containing the response text and host
     :raises RuntimeError: if the URL could not be fetched
     """
@@ -42,10 +43,29 @@ async def fetch(url: str, session: aiohttp.ClientSession) -> FetchResult:
             if response.status >= 400:
                 raise RuntimeError(f"Failed to fetch {url}: {response.status}")
 
+            # make a directory for any assets
+            relative_assets_path = f"{response.host}_assets"
+            try:
+                os.mkdir(os.path.join(output_path, relative_assets_path))
+            except FileExistsError:
+                pass
+
             text = await response.text()
-            parser = HTMLParser(text, url)
+            parser = HTMLParser(text, url, relative_assets_path)
             links = parser.get_links()
             images = parser.get_images()
+
+            for image in images:
+                url_filename = image.split("/")[-1]
+                async with session.get(image) as image_response:
+                    if image_response.status >= 400:
+                        raise RuntimeError(
+                            f"Failed to image fetch {image}: {image_response.status}")
+
+                    async with aiofiles.open(os.path.join(output_path, f"{relative_assets_path}/{url_filename}"), "wb") as f:
+                        await f.write(await image_response.read())
+
+            await write_to_file(os.path.join(output_path, f"{response.host}.html"), parser.replace_image_urls())
 
             return FetchResult(text, response.host, images, links)
     except Exception as e:
@@ -54,24 +74,20 @@ async def fetch(url: str, session: aiohttp.ClientSession) -> FetchResult:
 
 async def fetch_urls(urls: list[str], output_path: str) -> list[FetchResult]:
     """
-    Fetch a list of URLs and write the responses to files.
+    Fetch a list of URLs and save the pages to disk.
 
     :param urls: URLs to fetch
     :param output_path: path to folder to write files into
-    :param fetch_metadata: whether to fetch metadata
     :return: list of results
     """
     responses = []
 
     async with aiohttp.ClientSession() as session:
-        fetch_tasks = [fetch(url, session) for url in urls]
+        fetch_tasks = [fetch(url, output_path, session) for url in urls]
         for response in asyncio.as_completed(fetch_tasks):
             try:
                 response = await response
-                for image in response.images:
-                    print(f"Fetching {image}")
                 responses.append(response)
-                await write_to_file(f"{output_path}/{response.host}.html", response.body)
             except RuntimeError as e:
                 print(e)
 
--- web_fetch/html_parser.py
+++ web_fetch/html_parser.py
@@ -1,3 +1,4 @@
+import os
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
@@ -10,16 +11,18 @@ class HTMLParser:
     links for <a> and <img> tags.
     """
 
-    def __init__(self, html: str, base_url: str) -> None:
+    def __init__(self, html: str, base_url: str, assets_path: str) -> None:
         self.parsed = BeautifulSoup(html, "html.parser")
         self.base_url = base_url
+        self.assets_path = assets_path
 
     def get_links(self) -> list[str]:
         """
         Return a list of link URLs in the HTML. These are the `href` attributes
         of all `<a>` tags. Relative URLs are resolved using the base URL.
         """
-        urls = [link["href"] for link in self.parsed.find_all("a")]
+        urls = [link["href"]
+                for link in self.parsed.find_all("a", attrs={"href": True})]
         return [urljoin(self.base_url, url) for url in urls]
 
     def get_images(self) -> list[str]:
@@ -27,5 +30,18 @@ class HTMLParser:
         Return a list of image URLs in the HTML. These are the `src` attributes
         of all `<img>` tags. Relative URLs are resolved using the base URL.
         """
-        images = [image["src"] for image in self.parsed.find_all("img")]
+        images = [image["src"]
+                  for image in self.parsed.find_all("img", attrs={"src": True})]
         return [urljoin(self.base_url, image) for image in images]
+
+    def replace_image_urls(self) -> str:
+        """
+        Return a new HTML document with all the image `src` attributes replaced
+        with the new image URLs.
+        """
+        for image in self.parsed.find_all("img"):
+            # Build a relative path to the downloaded image
+            local_path = os.path.join(
+                self.assets_path, image["src"].split("/")[-1])
+            image["src"] = local_path
+        return self.parsed.prettify()
