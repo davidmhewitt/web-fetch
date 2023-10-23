565323505788d30d71de4e5147693382ec56940d
David Hewitt <davidmhewitt@users.noreply.github.com>
2023-10-23 20:28:37 +0000

    Fetch images concurrently

--- web_fetch/fetcher.py
+++ web_fetch/fetcher.py
@@ -27,6 +27,27 @@ async def write_to_file(filename: str, text: str) -> None:
         raise RuntimeError(f"Failed to write to {filename}: {e}") from e
 
 
+async def save_image(url: str, output_path: str, session: aiohttp.ClientSession) -> None:
+    """
+    Save an image from a URL to disk.
+
+    :param url: URL to fetch
+    :param output_path: path to folder to write files into
+    :param session: aiohttp session
+    :raises RuntimeError: if the URL could not be fetched
+    """
+    try:
+        async with session.get(url) as response:
+            if response.status >= 400:
+                raise RuntimeError(f"Failed to fetch {url}: {response.status}")
+
+            url_filename = url.split("/")[-1]
+            async with aiofiles.open(os.path.join(output_path, url_filename), "wb") as f:
+                await f.write(await response.read())
+    except Exception as e:
+        raise RuntimeError(f"Failed to fetch image {url}: {e}") from e
+
+
 async def fetch(url: str, output_path: str, session: aiohttp.ClientSession) -> FetchResult:
     """
     Fetch a URL and return a tuple of the response text and the HTTP host that
@@ -55,15 +76,13 @@ async def fetch(url: str, output_path: str, session: aiohttp.ClientSession) -> F
             links = parser.get_links()
             images = parser.get_images()
 
-            for image in images:
-                url_filename = image.split("/")[-1]
-                async with session.get(image) as image_response:
-                    if image_response.status >= 400:
-                        raise RuntimeError(
-                            f"Failed to image fetch {image}: {image_response.status}")
+            image_tasks = [save_image(image, os.path.join(
+                output_path, relative_assets_path), session) for image in images]
 
-                    async with aiofiles.open(os.path.join(output_path, f"{relative_assets_path}/{url_filename}"), "wb") as f:
-                        await f.write(await image_response.read())
+            image_results = await asyncio.gather(*image_tasks, return_exceptions=True)
+            for result in image_results:
+                if isinstance(result, Exception):
+                    print(result)
 
             await write_to_file(os.path.join(output_path, f"{response.host}.html"), parser.replace_image_urls())
 
@@ -84,11 +103,11 @@ async def fetch_urls(urls: list[str], output_path: str) -> list[FetchResult]:
 
     async with aiohttp.ClientSession() as session:
         fetch_tasks = [fetch(url, output_path, session) for url in urls]
-        for response in asyncio.as_completed(fetch_tasks):
-            try:
-                response = await response
-                responses.append(response)
-            except RuntimeError as e:
-                print(e)
+        fetch_results = await asyncio.gather(*fetch_tasks)
+        for result in fetch_results:
+            if isinstance(result, Exception):
+                print(result)
+            else:
+                responses.append(result)
 
     return responses
