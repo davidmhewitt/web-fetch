f60cb4b9fd6d81d8fa1e850b572020cab6b362f0
David Hewitt <davidmhewitt@users.noreply.github.com>
2023-10-23 15:41:19 +0000

    Run main async

--- web_fetch/cli.py
+++ web_fetch/cli.py
@@ -1,12 +1,14 @@
 import argparse
+import asyncio
 
-
-def cli():
+async def cli():
     parser = argparse.ArgumentParser()
     parser.add_argument("urls", nargs="+", help="URLs to fetch")
     args = parser.parse_args()
     print(args.urls)
 
+def main():
+    asyncio.run(cli())
 
 if __name__ == "__main__":
-    cli()
+    main()
